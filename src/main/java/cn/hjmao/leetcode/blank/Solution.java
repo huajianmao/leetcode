be7eb59a668ccdbb7b301b4a2dc89a407f611f59
Huajian Mao <huajianmao@gmail.com>
2017-01-17 20:56:41 +0800

    Add blank template for leetcode.

---
+++ src/main/java/cn/hjmao/leetcode/blank/Solution.java
@@ -0,0 +1,10 @@
+package cn.hjmao.leetcode.blank;
+
+/**
+ * Created by hjmao on 17/01/2017.
+ */
+
+public class Solution {
+
+  // FIXME: add implementation code here.
+}
---
+++ src/test/java/cn/hjmao/leetcode/blank/SolutionTest.java
@@ -0,0 +1,24 @@
+package cn.hjmao.leetcode.blank;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by hjmao on 17/01/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    this.solution = new Solution();
+  }
+
+  @AfterMethod
+  public void tearDown() throws Exception {
+
+  }
+
+  // FIXME: add test code here.
+}
