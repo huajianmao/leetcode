2175762843251b7432d1082b361536c881f58b16
Huajian Mao <huajianmao@gmail.com>
2017-01-18 21:59:46 +0800

    Add solution for longest palindromic substring problem.

---
+++ src/main/java/cn/hjmao/leetcode/longestpalindromicsubstring/Solution.java
@@ -0,0 +1,57 @@
+package cn.hjmao.leetcode.longestpalindromicsubstring;
+
+/**
+ * Created by hjmao on 17/01/2017.
+ *
+ * URL:
+ * =====
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ *
+ * Desc:
+ * =====
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ *
+ * Example:
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ *
+ * Example:
+ * Input: "cbbd"
+ * Output: "bb"
+ */
+
+public class Solution {
+  private int start;
+  private int length;
+
+  private void findLongestStringWithReverseOrder(String s, int i, int left, int right) {
+    for (int j = 0; j <= i; j++) {
+        if (left - j < 0 || right + j > s.length() - 1) {
+          break;
+        }
+        char prev = s.charAt(left - j);
+        char next = s.charAt(right + j);
+
+        if (prev != next) {
+          break;
+        }
+
+        if (this.length < right - left + 2 * j) {
+          this.start = left - j;
+          this.length = right - left + 2 * j;
+        }
+      }
+  }
+
+  public String longestPalindrome(String s) {
+    String palindrome = "";
+
+    for (int i = 0; i < s.length(); i++) {
+      findLongestStringWithReverseOrder(s, i, i, i + 1);
+      findLongestStringWithReverseOrder(s, i, i, i);
+    }
+
+    return s.substring(this.start, this.start + this.length + 1);
+  }
+}
---
+++ src/test/java/cn/hjmao/leetcode/longestpalindromicsubstring/SolutionTest.java
@@ -0,0 +1,76 @@
+package cn.hjmao.leetcode.longestpalindromicsubstring;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by hjmao on 18/01/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    this.solution = new Solution();
+  }
+
+  @Test
+  public void testLongestPalindrome1() throws Exception {
+    String s = "babad";
+    String expected1 = "bab";
+    String expected2 = "aba";
+
+    String palindrome = this.solution.longestPalindrome(s);
+    assertTrue(expected1.equals(palindrome) || expected2.equals(palindrome));
+  }
+
+  @Test
+  public void testLongestPalindrome2() throws Exception {
+    String s = "cbbd";
+    String expected = "bb";
+
+    assertEquals(this.solution.longestPalindrome(s), expected);
+  }
+
+  @Test
+  public void testLongestPalindrome3() throws Exception {
+    String s = "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc";
+    String expected = "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc";
+
+    assertEquals(this.solution.longestPalindrome(s), expected);
+  }
+
+  @Test
+  public void testLongestPalindromeMine1() throws Exception {
+    String s = "c";
+    String expected = "c";
+
+    assertEquals(this.solution.longestPalindrome(s), expected);
+  }
+
+  @Test
+  public void testLongestPalindromeMine2() throws Exception {
+    String s = "aaaaaaaaaaaaa";
+    String expected = "aaaaaaaaaaaaa";
+
+    assertEquals(this.solution.longestPalindrome(s), expected);
+  }
+
+  @Test
+  public void testLongestPalindromeMine3() throws Exception {
+    String s = "abcdefgfedcba";
+    String expected = "abcdefgfedcba";
+
+    assertEquals(this.solution.longestPalindrome(s), expected);
+  }
+
+  @Test
+  public void testLongestPalindromeMine4() throws Exception {
+    String s = "abcdeffedcba";
+    String expected = "abcdeffedcba";
+
+    assertEquals(this.solution.longestPalindrome(s), expected);
+  }
+}
