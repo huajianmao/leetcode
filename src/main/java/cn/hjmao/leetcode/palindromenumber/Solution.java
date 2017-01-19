b1f806f1dbf0ce9a831fc47436ee81b7d7c6ee92
Huajian Mao <huajianmao@gmail.com>
2017-01-19 21:57:53 +0800

    Add solution for palindrome number problem.

---
+++ src/main/java/cn/hjmao/leetcode/palindromenumber/Solution.java
@@ -0,0 +1,33 @@
+package cn.hjmao.leetcode.palindromenumber;
+
+/**
+ * Created by hjmao.
+ *
+ * URL:
+ * =====
+ * https://leetcode.com/problems/palindrome-number/
+ *
+ * Desc:
+ * =====
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ */
+
+public class Solution {
+  public boolean isPalindrome(int x) {
+    if (x < 0) {
+      return false;
+    }
+
+    boolean isPalindrome = false;
+
+    int left = x;
+    int reverse = 0;
+    while (left / 10 > 0) {
+      reverse = reverse * 10 + left % 10;
+      left = left / 10;
+    }
+    reverse = reverse * 10 + left % 10;
+
+    return x == reverse;
+  }
+}
