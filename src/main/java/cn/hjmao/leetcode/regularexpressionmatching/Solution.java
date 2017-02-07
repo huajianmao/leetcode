1eb1291562b84328224aeda1fc59e2582adfbf0f
Huajian Mao <huajianmao@gmail.com>
2017-02-07 16:07:24 +0800

    Add explanation for regular exp. matching solution.

--- src/main/java/cn/hjmao/leetcode/regularexpressionmatching/Solution.java
+++ src/main/java/cn/hjmao/leetcode/regularexpressionmatching/Solution.java
@@ -31,8 +31,20 @@ package cn.hjmao.leetcode.regularexpressionmatching;
 
 public class Solution {
 
-  // Solution by monkeyGoCrazy@Leetcode
-  // https://discuss.leetcode.com/topic/40371/easy-dp-java-solution-with-detailed-explanation
+  /**
+   * Solution by monkeyGoCrazy@Leetcode
+   * https://discuss.leetcode.com/topic/40371/easy-dp-java-solution-with-detailed-explanation
+   *
+   * 1, If p.charAt(j) == s.charAt(i) :  dp[i][j] = dp[i-1][j-1];
+   * 2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
+   * 3, If p.charAt(j) == '*':
+   *     here are two sub conditions:
+   *     1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
+   *     2   if p.charAt(i-1) == s.charAt(i) or p.charAt(i-1) == '.':
+   *             dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a
+   *          or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
+   *          or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+   */
   public boolean solutionByMonkeyGoCrazy(String s, String p) {
     if (s == null || p == null || p.startsWith("*")) {
       return false;
