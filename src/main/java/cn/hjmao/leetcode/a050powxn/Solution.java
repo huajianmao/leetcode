43bebdbf2328c4d6f3a85f2aa0fd559221a5d59c
Huajian Mao <huajianmao@gmail.com>
2017-02-08 19:17:32 +0800

    Add a binary search solution for pow(x, n)

--- src/main/java/cn/hjmao/leetcode/a050powxn/Solution.java
+++ src/main/java/cn/hjmao/leetcode/a050powxn/Solution.java
@@ -13,7 +13,7 @@ package cn.hjmao.leetcode.a050powxn;
  */
 
 public class Solution {
-  public double myPow(double x, int n) {
+  private double myPow1(double x, int n) {
     double sign = 1.0;
     if (x < 0 && n % 2 == 1) {
       sign = -1.0;
@@ -23,12 +23,12 @@ public class Solution {
     double pow = 1;
     if (n < 0) {
       if (n <= Integer.MIN_VALUE) {
-        return sign * 1.0 / myPow(x, -(n+1)) / x;
+        return sign * 1.0 / myPow1(x, -(n + 1)) / x;
       } else {
-        return sign * 1.0 / myPow(x, -n);
+        return sign * 1.0 / myPow1(x, -n);
       }
     } else {
-      for (int i=0; i<n; i++) {
+      for (int i = 0; i < n; i++) {
         if (pow * x - pow <= Double.MIN_VALUE && pow - pow * x <= Double.MIN_VALUE) {
           return sign * pow;
         }
@@ -38,10 +38,30 @@ public class Solution {
           return 0;
         }
         if (pow >= Double.MAX_VALUE || pow <= Double.NEGATIVE_INFINITY) {
-          return sign < 0 ? Double.NEGATIVE_INFINITY: Double.POSITIVE_INFINITY;
+          return sign < 0 ? Double.NEGATIVE_INFINITY : Double.POSITIVE_INFINITY;
         }
       }
       return sign * pow;
     }
+  }
+
+  private double myPow2(double x, int n) {
+    if (n == 0) {
+      return 1;
+    }
+    double temp = myPow2(x, n / 2);
+    if (n % 2 == 0) {
+      return temp * temp;
+    } else {
+      if (n > 0) {
+        return temp * temp * x;
+      } else {
+        return temp * temp / x;
+      }
+    }
+  }
+
+  public double myPow(double x, int n) {
+    return myPow2(x, n);
   }
 }
