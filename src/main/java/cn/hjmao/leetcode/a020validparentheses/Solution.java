f38e81ff73df6ffdee2d33d5f7c352f18021ff84
Huajian Mao <huajianmao@gmail.com>
2017-02-15 20:25:03 +0800

    Add solution for Valid Parentheses problem.

---
+++ src/main/java/cn/hjmao/leetcode/a020validparentheses/Solution.java
@@ -0,0 +1,47 @@
+package cn.hjmao.leetcode.a020validparentheses;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Created by hjmao.
+ *
+ * URL:
+ * =====
+ * https://leetcode.com/problems/valid-parentheses/
+ *
+ * Desc:
+ * =====
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ * The brackets must close in the correct order,
+ * "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+ */
+
+public class Solution {
+  private static final Map<Character, Character> CLOSE_PARENTHESES_MAP = new HashMap<>();
+  static {
+    CLOSE_PARENTHESES_MAP.put(')', '(');
+    CLOSE_PARENTHESES_MAP.put(']', '[');
+    CLOSE_PARENTHESES_MAP.put('}', '{');
+  }
+
+  public boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      char thisChar = s.charAt(i);
+      if (thisChar == '(' || thisChar == '[' || thisChar == '{') {
+          stack.push(thisChar);
+      } else if (thisChar == ')' || thisChar == ']' || thisChar == '}') {
+        if (stack.empty() || stack.pop() != CLOSE_PARENTHESES_MAP.get(thisChar)) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+    return stack.empty();
+  }
+}
+
---
+++ src/test/java/cn/hjmao/leetcode/a020validparentheses/SolutionTest.java
@@ -0,0 +1,72 @@
+package cn.hjmao.leetcode.a020validparentheses;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by hjmao on 15/02/2017.
+ */
+public class SolutionTest {
+  private Solution solution;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    this.solution = new Solution();
+  }
+
+  @Test
+  public void testIsValid1() throws Exception {
+    String s = "";
+    boolean expected = true;
+    boolean actual = this.solution.isValid(s);
+
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsValid2() throws Exception {
+    String s = "(";
+    boolean expected = false;
+    boolean actual = this.solution.isValid(s);
+
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsValid3() throws Exception {
+    String s = "()";
+    boolean expected = true;
+    boolean actual = this.solution.isValid(s);
+
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsValid4() throws Exception {
+    String s = "[]";
+    boolean expected = true;
+    boolean actual = this.solution.isValid(s);
+
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsValid5() throws Exception {
+    String s = "(())[]";
+    boolean expected = true;
+    boolean actual = this.solution.isValid(s);
+
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsValid6() throws Exception {
+    String s = "]";
+    boolean expected = false;
+    boolean actual = this.solution.isValid(s);
+
+    assertEquals(actual, expected);
+  }
+}
