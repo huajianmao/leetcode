329d3b5e30c239f057912e76955911ffa929a16c
Huajian Mao <huajianmao@gmail.com>
2017-02-28 22:53:00 +0800

    Add problem description for Count and Say problem.

---
+++ src/main/java/cn/hjmao/leetcode/a038countandsay/Solution.java
@@ -0,0 +1,39 @@
+package cn.hjmao.leetcode.a038countandsay;
+
+/**
+ * Created by hjmao.
+ *
+ * URL:
+ * =====
+ * https://leetcode.com/problems/count-and-say
+ *
+ * Desc:
+ * =====
+ * The count-and-say sequence is the sequence of integers beginning as follows:
+ * 1, 11, 21, 1211, 111221, ...
+ *
+ * 1 is read off as "one 1" or 11.
+ * 11 is read off as "two 1s" or 21.
+ * 21 is read off as "one 2, then one 1" or 1211.
+ *
+ * Given an integer n, generate the nth sequence.
+ *
+ * Note: The sequence of integers will be represented as a string.
+ */
+
+public class Solution {
+  public String countAndSay(int n) {
+    String result = null;
+//    Queue<Integer> queue = new LinkedList<>();
+//    queue.add(1);
+//
+//    for (int i = 0; i < n; i++) {
+//      int lastOne;
+//      while(queue.peek() != 0) {
+//
+//      }
+//    }
+
+    return result;
+  }
+}
