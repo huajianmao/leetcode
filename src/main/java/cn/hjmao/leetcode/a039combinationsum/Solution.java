c4d5e7dc9e3a8c78676a373af7a048ae6ccf4077
Huajian Mao <huajianmao@gmail.com>
2017-03-01 22:41:13 +0800

    Add test cases for Combination Sum problem.

---
+++ src/main/java/cn/hjmao/leetcode/a039combinationsum/Solution.java
@@ -0,0 +1,40 @@
+package cn.hjmao.leetcode.a039combinationsum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hjmao.
+ *
+ * URL:
+ * =====
+ * https://leetcode.com/problems/combination-sum
+ *
+ * Desc:
+ * =====
+ * Given a set of candidate numbers (C) (without duplicates) and a target number (T),
+ * find all unique combinations in C where the candidate numbers sums to T.
+ *
+ * The same repeated number may be chosen from C unlimited number of times.
+ *
+ * Note:
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ *
+ * For example, given candidate set [2, 3, 6, 7] and target 7,
+ * A solution set is:
+ * [
+ *   [7],
+ *   [2, 2, 3]
+ * ]
+ */
+
+public class Solution {
+  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    List<List<Integer>> combinations = new ArrayList<>();
+
+    // TODO
+
+    return combinations;
+  }
+}
---
+++ src/test/java/cn/hjmao/leetcode/a039combinationsum/SolutionTest.java
@@ -0,0 +1,50 @@
+package cn.hjmao.leetcode.a039combinationsum;
+
+import cn.hjmao.utils.AssertUtils;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by hjmao on 01/03/2017.
+ */
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testCombinationSum1() throws Exception {
+    int[] candidates = {2};
+    int target = 3;
+    List<List<Integer>> expected = Arrays.asList();
+    List<List<Integer>> actual = this.solution.combinationSum(candidates, target);
+    AssertUtils.assertEqualsOfTwoArrays(actual, expected);
+  }
+
+  @Test
+  public void testCombinationSum3() throws Exception {
+    int[] candidates = {2, 3, 6, 7};
+    int target = 7;
+    List<List<Integer>> expected = Arrays.asList(
+        Arrays.asList(7),
+        Arrays.asList(2, 2, 4)
+    );
+    List<List<Integer>> actual = this.solution.combinationSum(candidates, target);
+    AssertUtils.assertEqualsOfTwoArrays(actual, expected);
+  }
+
+  @Test
+  public void testCombinationSum2() throws Exception {
+    int[] candidates = {2, 3, 6, 7};
+    int target = 6;
+    List<List<Integer>> expected = Arrays.asList(
+        Arrays.asList(6),
+        Arrays.asList(3, 3),
+        Arrays.asList(2, 2, 2)
+    );
+    List<List<Integer>> actual = this.solution.combinationSum(candidates, target);
+    AssertUtils.assertEqualsOfTwoArrays(actual, expected);
+  }
+}
