bee426e0e0b301ed6839ed68eab7b4e5993b4f7a
Huajian Mao <huajianmao@gmail.com>
2017-03-02 10:51:55 +0800

    Add answer for Combination Sum.

--- src/main/java/cn/hjmao/leetcode/a039combinationsum/Solution.java
+++ src/main/java/cn/hjmao/leetcode/a039combinationsum/Solution.java
@@ -1,7 +1,6 @@
 package cn.hjmao.leetcode.a039combinationsum;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by hjmao.
@@ -30,11 +29,90 @@ import java.util.List;
  */
 
 public class Solution {
-  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+  private List<List<Integer>> combinationSumInRange(int[] candidates, int target, int length) {
     List<List<Integer>> combinations = new ArrayList<>();
 
-    // TODO
+    if (length == 1) {
+      if (target % candidates[0] == 0) {
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < target / candidates[0]; i++) {
+          temp.add(candidates[0]);
+        }
+        combinations.add(temp);
+      }
+      return combinations;
+    }
+
+    if (candidates[length - 1] > target) {
+      return combinationSumInRange(candidates, target, length - 1);
+    } else {
+      int thisOne = candidates[length - 1];
+
+      if (thisOne == target) {
+        List<Integer> temp = new ArrayList<>();
+        temp.add(target);
+        combinations.add(temp);
+      } else {
+        for (int i = 1; i < target / thisOne + 1; i++) {
+          List<List<Integer>> temp = combinationSumInRange(candidates, target - thisOne * i, length);
+          if (temp.size() > 0) {
+            List<Integer> moreIntegers = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+              moreIntegers.add(thisOne);
+            }
+            for (List<Integer> available : temp) {
+              available.addAll(moreIntegers);
+              combinations.add(available);
+            }
+          }
+        }
+      }
+      combinations.addAll(combinationSumInRange(candidates, target, length - 1));
+    }
 
     return combinations;
+  }
+
+  public List<List<Integer>> combinationSumUgly(int[] candidates, int target) {
+    Arrays.sort(candidates);
+    List<List<Integer>> results = combinationSumInRange(candidates, target, candidates.length);
+    List<List<Integer>> deduplicated = new ArrayList<>();
+    Set<String> temp = new HashSet<>();
+    for (List<Integer> result: results) {
+      StringBuilder stringBuilder = new StringBuilder();
+      for (Integer value: result) {
+        stringBuilder.append(value);
+        stringBuilder.append(",");
+      }
+      String thisKey = stringBuilder.toString();
+      if (!temp.contains(thisKey)) {
+        temp.add(thisKey);
+        deduplicated.add(result);
+      }
+    }
+
+    return deduplicated;
+  }
+
+
+  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    Arrays.sort(candidates);
+    List<List<Integer>> result = new ArrayList<>();
+    dfs(result, new LinkedList<>(), candidates, target);
+    return result;
+  }
+
+  private void dfs(List<List<Integer>> result, LinkedList<Integer> list, int[] arr, int target) {
+    if (target == 0) {
+      result.add(new LinkedList<>(list));
+      return;
+    }
+    for (int i = arr.length - 1; i >= 0; i--) {
+      if (arr[i] <= target) {
+        list.addFirst(arr[i]);
+        dfs(result, list, Arrays.copyOfRange(arr, 0, i + 1), target - arr[i]);
+        list.removeFirst();
+      }
+    }
   }
 }
--- src/test/java/cn/hjmao/leetcode/a039combinationsum/SolutionTest.java
+++ src/test/java/cn/hjmao/leetcode/a039combinationsum/SolutionTest.java
@@ -24,25 +24,49 @@ public class SolutionTest {
   }
 
   @Test
-  public void testCombinationSum3() throws Exception {
-    int[] candidates = {2, 3, 6, 7};
-    int target = 7;
+  public void testCombinationSum2() throws Exception {
+    int[] candidates = {2, 4};
+    int target = 4;
     List<List<Integer>> expected = Arrays.asList(
-        Arrays.asList(7),
-        Arrays.asList(2, 2, 4)
+        Arrays.asList(4),
+        Arrays.asList(2, 2)
     );
     List<List<Integer>> actual = this.solution.combinationSum(candidates, target);
     AssertUtils.assertEqualsOfTwoArrays(actual, expected);
   }
 
   @Test
-  public void testCombinationSum2() throws Exception {
+  public void testCombinationSum3() throws Exception {
+    int[] candidates = {2, 3, 6, 7};
+    int target = 7;
+    List<List<Integer>> expected = Arrays.asList(
+        Arrays.asList(7),
+        Arrays.asList(2, 2, 3)
+    );
+    List<List<Integer>> actual = this.solution.combinationSum(candidates, target);
+    AssertUtils.assertEqualsOfTwoArrays(actual, expected);
+  }
+
+  @Test
+  public void testCombinationSum4() throws Exception {
     int[] candidates = {2, 3, 6, 7};
     int target = 6;
     List<List<Integer>> expected = Arrays.asList(
         Arrays.asList(6),
         Arrays.asList(3, 3),
         Arrays.asList(2, 2, 2)
+    );
+    List<List<Integer>> actual = this.solution.combinationSum(candidates, target);
+    AssertUtils.assertEqualsOfTwoArrays(actual, expected);
+  }
+
+  @Test
+  public void testCombinationSum5() throws Exception {
+    int[] candidates = {3, 5, 7};
+    int target = 10;
+    List<List<Integer>> expected = Arrays.asList(
+        Arrays.asList(3, 7),
+        Arrays.asList(5, 5)
     );
     List<List<Integer>> actual = this.solution.combinationSum(candidates, target);
     AssertUtils.assertEqualsOfTwoArrays(actual, expected);
