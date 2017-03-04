d359e2c601cf33614acf7f158d7de6aed24e948c
Huajian Mao <huajianmao@gmail.com>
2017-03-04 23:39:46 +0800

    Finish Combination Sum II problem.

--- src/main/java/cn/hjmao/leetcode/a040combinationsum2/Solution.java
+++ src/main/java/cn/hjmao/leetcode/a040combinationsum2/Solution.java
@@ -1,6 +1,7 @@
 package cn.hjmao.leetcode.a040combinationsum2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -32,9 +33,35 @@ import java.util.List;
  */
 
 public class Solution {
-  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+  private List<List<Integer>> combinate(int[] candidates, int target, int length) {
     List<List<Integer>> combinations = new ArrayList<>();
 
+    List<List<Integer>> hasMeLists = new ArrayList<>();
+    if (target == candidates[length - 1]) {
+      hasMeLists.add(new ArrayList<>());
+    } else if (candidates[length - 1] < target) {
+      if (length > 1) {
+        hasMeLists = combinate(candidates, target - candidates[length - 1], length - 1);
+      }
+    }
+    for (List<Integer> listHasMe: hasMeLists) {
+      listHasMe.add(candidates[length - 1]);
+      combinations.add(listHasMe);
+    }
+
+    while (length > 1 && candidates[length - 1] == candidates[length - 2]) {
+      length--;
+    }
+    if (length > 1) {
+      List<List<Integer>> listNotHasMe = combinate(candidates, target, length - 1);
+      combinations.addAll(listNotHasMe);
+    }
+
     return combinations;
+  }
+
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    Arrays.sort(candidates);
+    return combinate(candidates, target, candidates.length);
   }
 }
