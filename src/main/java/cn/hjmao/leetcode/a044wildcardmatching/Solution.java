4ec9794752a68ad1d82b2ce73cd9ba3ebe17ec63
Huajian Mao <huajianmao@gmail.com>
2017-03-09 17:20:55 +0800

    Add problem description for Wildcard Matching.

---
+++ src/main/java/cn/hjmao/leetcode/a044wildcardmatching/Solution.java
@@ -0,0 +1,38 @@
+package cn.hjmao.leetcode.a044wildcardmatching;
+
+/**
+ * Created by hjmao.
+ *
+ * URL:
+ * =====
+ * https://leetcode.com/problems/wildcard-matching
+ *
+ * Desc:
+ * =====
+ * Implement wildcard pattern matching with support for '?' and '*'.
+ *
+ * '?' Matches any single character.
+ * '*' Matches any sequence of characters (including the empty sequence).
+ *
+ * The matching should cover the entire input string (not partial).
+ *
+ * The function prototype should be:
+ * bool isMatch(const char *s, const char *p)
+ *
+ * Some examples:
+ * isMatch("aa","a") → false
+ * isMatch("aa","aa") → true
+ * isMatch("aaa","aa") → false
+ * isMatch("aa", "*") → true
+ * isMatch("aa", "a*") → true
+ * isMatch("ab", "?*") → true
+ * isMatch("aab", "c*a*b") → false
+ */
+
+public class Solution {
+  public boolean isMatch(String s, String p) {
+    boolean matched = false;
+    // TODO:
+    return matched;
+  }
+}
---
+++ src/test/java/cn/hjmao/leetcode/a044wildcardmatching/SolutionTest.java
@@ -0,0 +1,75 @@
+package cn.hjmao.leetcode.a044wildcardmatching;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by hjmao on 17-3-9.
+ */
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testIsMatch1() throws Exception {
+    String s = "aa";
+    String p = "a";
+    boolean expected = false;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch2() throws Exception {
+    String s = "aa";
+    String p = "aa";
+    boolean expected = true;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch3() throws Exception {
+    String s = "aaa";
+    String p = "aa";
+    boolean expected = false;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch4() throws Exception {
+    String s = "aa";
+    String p = "*";
+    boolean expected = true;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch5() throws Exception {
+    String s = "aa";
+    String p = "a*";
+    boolean expected = true;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch6() throws Exception {
+    String s = "ab";
+    String p = "?*";
+    boolean expected = true;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch7() throws Exception {
+    String s = "aab";
+    String p = "c*a*b";
+    boolean expected = false;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+}
