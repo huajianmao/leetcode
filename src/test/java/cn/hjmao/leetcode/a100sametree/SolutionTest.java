49c4ed67398f1ff8679159d0af3ed26262cf832e
Huajian Mao <huajianmao@gmail.com>
2019-03-09 11:31:36 +0800

    Add solution for Problem 100

---
+++ src/test/java/cn/hjmao/leetcode/a100sametree/SolutionTest.java
@@ -0,0 +1,13 @@
+package cn.hjmao.leetcode.a100sametree;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+    Solution solution = new Solution();
+
+    @Test
+    public void testIsSameTree() {
+    }
+}
