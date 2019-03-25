02b0fdd1b534d38ef30b1f941df17739ca6bfb1e
Huajian Mao <huajianmao@gmail.com>
2019-03-26 07:36:00 +0800

    Add solution for problem 44 [Time limit exceeded]

--- src/main/java/cn/hjmao/leetcode/a044wildcardmatching/Solution.java
+++ src/main/java/cn/hjmao/leetcode/a044wildcardmatching/Solution.java
@@ -31,8 +31,32 @@ package cn.hjmao.leetcode.a044wildcardmatching;
 
 public class Solution {
   public boolean isMatch(String s, String p) {
-    boolean matched = false;
-    // TODO:
-    return matched;
+    if (s.length() == 0) {
+      for (int i = 0; i < p.length(); i++) {
+        if (p.charAt(i) != '*') {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      char first = s.charAt(0);
+      if (p.length() == 0) {
+        return false;
+      } else {
+        if (p.charAt(0) != '?' && p.charAt(0) != '*' && p.charAt(0) != first) {
+          return false;
+        } else if (p.charAt(0) == first) {
+          return isMatch(s.substring(1, s.length()), p.substring(1, p.length()));
+        } else if (p.charAt(0) == '?') {
+          return isMatch(s.substring(1, s.length()), p.substring(1, p.length()));
+        } else if (p.charAt(0) == '*') {
+          return isMatch(s.substring(1, s.length()), p.substring(1, p.length()))
+                  || isMatch(s.substring(1, s.length()), p)
+                  || isMatch(s, p.substring(1, p.length()));
+        } else {
+          return false;
+        }
+      }
+    }
   }
 }
--- src/test/java/cn/hjmao/leetcode/a044wildcardmatching/SolutionTest.java
+++ src/test/java/cn/hjmao/leetcode/a044wildcardmatching/SolutionTest.java
@@ -19,56 +19,92 @@ public class SolutionTest {
     assertEquals(actual, expected);
   }
 
-//  @Test
-//  public void testIsMatch2() throws Exception {
-//    String s = "aa";
-//    String p = "aa";
-//    boolean expected = true;
-//    boolean actual = this.solution.isMatch(s, p);
-//    assertEquals(actual, expected);
-//  }
+  @Test
+  public void testIsMatch2() throws Exception {
+    String s = "aa";
+    String p = "aa";
+    boolean expected = true;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch3() throws Exception {
+    String s = "aaa";
+    String p = "aa";
+    boolean expected = false;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch4() throws Exception {
+    String s = "aa";
+    String p = "*";
+    boolean expected = true;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch5() throws Exception {
+    String s = "aa";
+    String p = "a*";
+    boolean expected = true;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch6() throws Exception {
+    String s = "ab";
+    String p = "?*";
+    boolean expected = true;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch7() throws Exception {
+    String s = "aab";
+    String p = "c*a*b";
+    boolean expected = false;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch8() throws Exception {
+    String s = "adceb";
+    String p = "*a*b";
+    boolean expected = true;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatch9() throws Exception {
+    String s = "acdcb";
+    String p = "a*c?b";
+    boolean expected = false;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testIsMatchx10() throws Exception {
+    String s = "cb";
+    String p = "c?b";
+    boolean expected = false;
+    boolean actual = this.solution.isMatch(s, p);
+    assertEquals(actual, expected);
+  }
 //
 //  @Test
-//  public void testIsMatch3() throws Exception {
-//    String s = "aaa";
-//    String p = "aa";
-//    boolean expected = false;
-//    boolean actual = this.solution.isMatch(s, p);
-//    assertEquals(actual, expected);
-//  }
-//
-//  @Test
-//  public void testIsMatch4() throws Exception {
-//    String s = "aa";
-//    String p = "*";
-//    boolean expected = true;
-//    boolean actual = this.solution.isMatch(s, p);
-//    assertEquals(actual, expected);
-//  }
-//
-//  @Test
-//  public void testIsMatch5() throws Exception {
-//    String s = "aa";
-//    String p = "a*";
-//    boolean expected = true;
-//    boolean actual = this.solution.isMatch(s, p);
-//    assertEquals(actual, expected);
-//  }
-//
-//  @Test
-//  public void testIsMatch6() throws Exception {
-//    String s = "ab";
-//    String p = "?*";
-//    boolean expected = true;
-//    boolean actual = this.solution.isMatch(s, p);
-//    assertEquals(actual, expected);
-//  }
-//
-//  @Test
-//  public void testIsMatch7() throws Exception {
-//    String s = "aab";
-//    String p = "c*a*b";
-//    boolean expected = false;
+//  public void testIsMatchx11() throws Exception {
+//    String s = "babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab";
+//    String p = "***bba**a*bbba**aab**b";
+//    boolean expected = ???;
 //    boolean actual = this.solution.isMatch(s, p);
 //    assertEquals(actual, expected);
 //  }
