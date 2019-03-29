c31641fe191bae3594fadd77990033586db62280
Huajian Mao <huajianmao@gmail.com>
2019-03-30 07:56:20 +0800

    Add solution for problem 136

---
+++ src/test/java/cn/hjmao/leetcode/a136singlenumber/SolutionTest.java
@@ -0,0 +1,26 @@
+package cn.hjmao.leetcode.a136singlenumber;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @Test
+    public void testSingleNumber1() {
+        int[] nums = {2, 2, 1};
+        int expect = 1;
+        int actual = solution.singleNumber(nums);
+        assertEquals(actual, expect);
+    }
+
+    @Test
+    public void testSingleNumber2() {
+        int[] nums = {4, 1, 2, 1, 2};
+        int expect = 4;
+        int actual = solution.singleNumber(nums);
+        assertEquals(actual, expect);
+    }
+}
