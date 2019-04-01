f1345bc766f993db43a23828be1266e6d9a920fb
Huajian Mao <huajianmao@gmail.com>
2019-04-01 11:16:07 +0800

    Add solution for problem 137

---
+++ src/test/java/cn/hjmao/leetcode/a137singlenumberii/SolutionTest.java
@@ -0,0 +1,25 @@
+package cn.hjmao.leetcode.a137singlenumberii;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testSingleNumber1() {
+    int[] nums = {2, 2, 3, 2};
+    int expect = 3;
+    int actual = solution.singleNumber(nums);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testSingleNumber2() {
+    int[] nums = {0, 1, 0, 1, 0, 1, 99};
+    int expect = 99;
+    int actual = solution.singleNumber(nums);
+    assertEquals(actual, expect);
+  }
+}
