ac355c97be816f6ddeb18901f904dd7a63d399fb
Huajian Mao <huajianmao@gmail.com>
2019-04-03 15:58:22 +0800

    Add solutions for problem 160

---
+++ src/test/java/cn/hjmao/leetcode/a160intersectionoftwolinkedlists/SolutionTest.java
@@ -0,0 +1,22 @@
+package cn.hjmao.leetcode.a160intersectionoftwolinkedlists;
+
+import cn.hjmao.utils.list.ListNode;
+import cn.hjmao.utils.list.ListNodeUtils;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+
+  Solution solution = new Solution();
+
+  @Test
+  public void testGetIntersectionNode() {
+    int[] arrayA = {4, 1, 8, 4, 5};
+    ListNode headA = ListNodeUtils.createListFromArray(arrayA);
+    int[] arrayB = {5, 0, 1, 8, 4, 5};
+    ListNode headB = ListNodeUtils.createListFromArray(arrayB);
+
+    solution.getIntersectionNode(headA, headB);
+  }
+}
