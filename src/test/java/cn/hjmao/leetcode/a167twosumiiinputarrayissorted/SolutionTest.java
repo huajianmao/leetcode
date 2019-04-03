12597208248341ed587c32188dc46223c1724643
Huajian Mao <huajianmao@gmail.com>
2019-04-04 07:16:10 +0800

    Add solution for problem 167

---
+++ src/test/java/cn/hjmao/leetcode/a167twosumiiinputarrayissorted/SolutionTest.java
@@ -0,0 +1,20 @@
+package cn.hjmao.leetcode.a167twosumiiinputarrayissorted;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+
+  Solution solution = new Solution();
+
+  @Test
+  public void testTwoSum1() {
+    int[] numbers = {2, 7, 11, 15};
+    int target = 9;
+
+    int[] expected = {1, 2};
+    int[] actual = solution.twoSum(numbers, target);
+    assertEquals(actual, expected);
+  }
+}
