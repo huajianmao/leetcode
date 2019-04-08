b2916a1e220b3e313b3331926ee7e58ee5beab5f
Huajian Mao <huajianmao@gmail.com>
2019-04-08 21:38:29 +0800

    Add solution for problem 172

---
+++ src/test/java/cn/hjmao/leetcode/a172factorialtrailingzeroes/SolutionTest.java
@@ -0,0 +1,27 @@
+package cn.hjmao.leetcode.a172factorialtrailingzeroes;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testTrailingZeroes1() {
+    int n = 3;
+    int expect = 0;
+
+    int actual = solution.trailingZeroes(n);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testTrailingZeroes2() {
+    int n = 5;
+    int expect = 1;
+
+    int actual = solution.trailingZeroes(n);
+    assertEquals(actual, expect);
+  }
+}
