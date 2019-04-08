09f65192cf53df3bff12a50bd04b19a4fb01e3be
Huajian Mao <huajianmao@gmail.com>
2019-04-08 21:19:41 +0800

    Add solution for problem 169

---
+++ src/test/java/cn/hjmao/leetcode/a169majorityelement/SolutionTest.java
@@ -0,0 +1,28 @@
+package cn.hjmao.leetcode.a169majorityelement;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+
+  Solution solution = new Solution();
+
+  @Test
+  public void testMajorityElement1() {
+    int[] nums = {3, 2, 3};
+    int expect = 3;
+
+    int actual = solution.majorityElement(nums);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testMajorityElement2() {
+    int[] nums = {2, 2, 1, 1, 1, 2, 2};
+    int expect = 2;
+
+    int actual = solution.majorityElement(nums);
+    assertEquals(actual, expect);
+  }
+}
