021e94511c9b6b3fb03dfc6e39baaee33e842735
Huajian Mao <huajianmao@gmail.com>
2019-04-10 08:42:49 +0800

    Add solution fgor problem 190

---
+++ src/test/java/cn/hjmao/leetcode/a190reversebits/SolutionTest.java
@@ -0,0 +1,26 @@
+package cn.hjmao.leetcode.a190reversebits;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+
+  Solution solution = new Solution();
+
+  @Test
+  public void testReverseBits1() {
+    int n = 43261596;
+    int expect = 964176192;
+    int actual = solution.reverseBits(n);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testReverseBits2() {
+    int n = -3;
+    int expect = -1073741825;
+    int actual = solution.reverseBits(n);
+    assertEquals(actual, expect);
+  }
+}
