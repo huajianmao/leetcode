ae10f5dc4cbf74e1543fa32c73bf9e66a7c8559e
Huajian Mao <huajianmao@gmail.com>
2019-04-17 07:48:59 +0800

    Add solution for problem 217

---
+++ src/test/java/cn/hjmao/leetcode/a217containsduplicate/SolutionTest.java
@@ -0,0 +1,37 @@
+package cn.hjmao.leetcode.a217containsduplicate;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testContainsDuplicate1() {
+    int[] nums = {1, 2, 3, 1};
+    boolean actual = solution.containsDuplicate(nums);
+    assertTrue(actual);
+  }
+
+  @Test
+  public void testContainsDuplicate2() {
+    int[] nums = {1, 2, 3, 4};
+    boolean actual = solution.containsDuplicate(nums);
+    assertFalse(actual);
+  }
+
+  @Test
+  public void testContainsDuplicate3() {
+    int[] nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+    boolean actual = solution.containsDuplicate(nums);
+    assertTrue(actual);
+  }
+
+  @Test
+  public void testContainsDuplicate4() {
+    int[] nums = {3, 3};
+    boolean actual = solution.containsDuplicate(nums);
+    assertTrue(actual);
+  }
+}
