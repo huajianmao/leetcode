736307a30c80b464d675a3f27e3e91c349bd572b
Huajian Mao <huajianmao@gmail.com>
2019-04-21 13:44:34 +0800

    Add solution for problem 242

---
+++ src/test/java/cn/hjmao/leetcode/a242validanagram/SolutionTest.java
@@ -0,0 +1,25 @@
+package cn.hjmao.leetcode.a242validanagram;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testIsAnagram1() {
+    String s = "anagram";
+    String t = "nagaram";
+    boolean actual = solution.isAnagram(s, t);
+    assertTrue(actual);
+  }
+
+  @Test
+  public void testIsAnagram2() {
+    String s = "rat";
+    String t = "car";
+    boolean actual = solution.isAnagram(s, t);
+    assertFalse(actual);
+  }
+}
