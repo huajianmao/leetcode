47cd8acf1e0ac05d024707f6f8348c3772874be3
Huajian Mao <huajianmao@gmail.com>
2019-04-23 08:24:30 +0800

    Add solution for problem 263

---
+++ src/test/java/cn/hjmao/leetcode/a263uglynumber/SolutionTest.java
@@ -0,0 +1,45 @@
+package cn.hjmao.leetcode.a263uglynumber;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testIsUgly1() {
+    int num = 1;
+    boolean actual = solution.isUgly(num);
+    assertTrue(actual);
+  }
+
+  @Test
+  public void testIsUgly2() {
+    int num = 2;
+    boolean actual = solution.isUgly(num);
+    assertTrue(actual);
+  }
+
+  @Test
+  public void testIsUgly3() {
+    int num = 6;
+    boolean actual = solution.isUgly(num);
+    assertTrue(actual);
+  }
+
+
+  @Test
+  public void testIsUgly4() {
+    int num = 8;
+    boolean actual = solution.isUgly(num);
+    assertTrue(actual);
+  }
+
+  @Test
+  public void testIsUgly5() {
+    int num = 14;
+    boolean actual = solution.isUgly(num);
+    assertFalse(actual);
+  }
+}
