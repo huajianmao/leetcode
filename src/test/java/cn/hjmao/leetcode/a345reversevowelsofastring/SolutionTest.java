83cd527c623d0ce1fd8861f77c4b6cfd0e79f51e
Huajian Mao <huajianmao@gmail.com>
2019-04-24 14:21:57 +0800

    Add solution for problem 345

---
+++ src/test/java/cn/hjmao/leetcode/a345reversevowelsofastring/SolutionTest.java
@@ -0,0 +1,49 @@
+package cn.hjmao.leetcode.a345reversevowelsofastring;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testReverseVowels1() {
+    String s = "hello";
+    String expect = "holle";
+    String actual = solution.reverseVowels(s);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testReverseVowels2() {
+    String s = "leetcode";
+    String expect = "leotcede";
+    String actual = solution.reverseVowels(s);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testReverseVowels3() {
+    String s = "OE";
+    String expect = "EO";
+    String actual = solution.reverseVowels(s);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testReverseVowels4() {
+    String s = "cd";
+    String expect = "cd";
+    String actual = solution.reverseVowels(s);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testReverseVowels5() {
+    String s = " ";
+    String expect = " ";
+    String actual = solution.reverseVowels(s);
+    assertEquals(actual, expect);
+  }
+}
