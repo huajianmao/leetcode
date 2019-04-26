f703c639fc3d36a70558f65a848b60ac98bdf726
Huajian Mao <huajianmao@gmail.com>
2019-04-26 18:13:39 +0800

    Add solution for problem 383

---
+++ src/test/java/cn/hjmao/leetcode/a383ransomnote/SolutionTest.java
@@ -0,0 +1,49 @@
+package cn.hjmao.leetcode.a383ransomnote;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testCanConstruct1() {
+    String ransomNote = "a";
+    String magazine = "b";
+    boolean actual = solution.canConstruct(ransomNote, magazine);
+    assertFalse(actual);
+  }
+
+  @Test
+  public void testCanConstruct2() {
+    String ransomNote = "aa";
+    String magazine = "ab";
+    boolean actual = solution.canConstruct(ransomNote, magazine);
+    assertFalse(actual);
+  }
+
+  @Test
+  public void testCanConstruct3() {
+    String ransomNote = "aa";
+    String magazine = "aab";
+    boolean actual = solution.canConstruct(ransomNote, magazine);
+    assertTrue(actual);
+  }
+
+  @Test
+  public void testCanConstruct4() {
+    String ransomNote = "";
+    String magazine = "b";
+    boolean actual = solution.canConstruct(ransomNote, magazine);
+    assertTrue(actual);
+  }
+
+  @Test
+  public void testCanConstruct6() {
+    String ransomNote = "a";
+    String magazine = "";
+    boolean actual = solution.canConstruct(ransomNote, magazine);
+    assertFalse(actual);
+  }
+}
