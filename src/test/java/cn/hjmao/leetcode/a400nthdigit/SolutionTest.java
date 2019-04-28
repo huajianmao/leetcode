ca8efce550a5e24be8e918b731afa0932f28f942
Huajian Mao <huajianmao@gmail.com>
2019-04-28 09:45:57 +0800

    Add solution for problem 400

---
+++ src/test/java/cn/hjmao/leetcode/a400nthdigit/SolutionTest.java
@@ -0,0 +1,41 @@
+package cn.hjmao.leetcode.a400nthdigit;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testFindNthDigit1() {
+    int n = 3;
+    int expect = 3;
+    int actual = solution.findNthDigit(n);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testFindNthDigit2() {
+    int n = 11;
+    int expect = 0;
+    int actual = solution.findNthDigit(n);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testFindNthDigit3() {
+    int n = 10;
+    int expect = 1;
+    int actual = solution.findNthDigit(n);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testFindNthDigit4() {
+    int n = 1000000000;
+    int expect = 1;
+    int actual = solution.findNthDigit(n);
+    assertEquals(actual, expect);
+  }
+}
