0de36a655f1861fb9094d225369728cb55c4b4d8
Huajian Mao <huajianmao@gmail.com>
2019-05-03 14:23:57 +0800

    Add solution for problem 448

---
+++ src/test/java/cn/hjmao/leetcode/a448findallnumbersdisappearedinanarray/SolutionTest.java
@@ -0,0 +1,29 @@
+package cn.hjmao.leetcode.a448findallnumbersdisappearedinanarray;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static org.testng.Assert.assertEquals;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testFindDisappearedNumbers1() {
+    int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+    List<Integer> expect = Arrays.asList(5, 6);
+    List<Integer> actual = solution.findDisappearedNumbers(nums);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testFindDisappearedNumbers2() {
+    int[] nums = {4, 3, 2, 4, 8, 2, 3, 1};
+    List<Integer> expect = Arrays.asList(5, 6, 7);
+    List<Integer> actual = solution.findDisappearedNumbers(nums);
+    assertEquals(actual, expect);
+  }
+}
