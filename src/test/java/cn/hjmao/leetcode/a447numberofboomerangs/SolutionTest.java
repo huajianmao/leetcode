ae84f3a63b4a5ab1b4fc44aadf00f18c46ea52d0
Huajian Mao <huajianmao@gmail.com>
2019-05-03 10:16:21 +0800

    Add solution for problem 447

---
+++ src/test/java/cn/hjmao/leetcode/a447numberofboomerangs/SolutionTest.java
@@ -0,0 +1,17 @@
+package cn.hjmao.leetcode.a447numberofboomerangs;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testNumberOfBoomerangs() {
+    int[][] points = {{0, 0}, {1, 0}, {2, 0}};
+    int expect = 2;
+    int actual = solution.numberOfBoomerangs(points);
+    assertEquals(actual, expect);
+  }
+}
