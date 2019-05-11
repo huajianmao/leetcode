a29215b2db1e53596549b301d53e334bccf078c8
Huajian Mao <huajianmao@gmail.com>
2019-05-11 22:00:19 +0800

    Add solution for problem 507

---
+++ src/test/java/cn/hjmao/leetcode/a507perfectnumber/SolutionTest.java
@@ -0,0 +1,24 @@
+package cn.hjmao.leetcode.a507perfectnumber;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testCheckPerfectNumber1() {
+    int num = 28;
+    boolean actual = solution.checkPerfectNumber(num);
+    assertTrue(actual);
+  }
+
+  @Test
+  public void testCheckPerfectNumber2() {
+    int num = 100000000;
+    boolean actual = solution.checkPerfectNumber(num);
+    assertFalse(actual);
+  }
+}
