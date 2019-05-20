9cfca8905a31c075d5bbb4c1d433b0d7fdd73058
Huajian Mao <huajianmao@gmail.com>
2019-05-20 22:33:17 +0800

    Add solution for problem 575

---
+++ src/test/java/cn/hjmao/leetcode/a575distributecandies/SolutionTest.java
@@ -0,0 +1,33 @@
+package cn.hjmao.leetcode.a575distributecandies;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+
+  @Test
+  public void testDistributeCandies1() {
+    int[] candies = {1, 1, 2, 2, 3, 3};
+    int expect = 3;
+    int actual = solution.distributeCandies(candies);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testDistributeCandies2() {
+    int[] candies = {1, 1, 2, 3};
+    int expect = 2;
+    int actual = solution.distributeCandies(candies);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testDistributeCandies3() {
+    int[] candies = {1, 2, 4, 3};
+    int expect = 2;
+    int actual = solution.distributeCandies(candies);
+    assertEquals(actual, expect);
+  }
+}
