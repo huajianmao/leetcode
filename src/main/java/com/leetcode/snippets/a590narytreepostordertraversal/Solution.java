20fe3923ce2b45794dc7ae7c5f11a8fbfd199281
Huajian Mao <huajianmao@gmail.com>
2019-05-21 09:07:25 +0800

    Add solution for problem 590

---
+++ src/main/java/com/leetcode/snippets/a590narytreepostordertraversal/Solution.java
@@ -0,0 +1,40 @@
+package com.leetcode.snippets.a590narytreepostordertraversal;
+
+import cn.hjmao.utils.narytree.Node;
+
+import java.util.List;
+
+/**
+ * Created by hjmao.
+ *
+ * URL:
+ * =====
+ * https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+ *
+ * Desc:
+ * =====
+ * Given an n-ary tree, return the postorder traversal of its nodes' values.
+ * 
+ * 
+ * For example, given a 3-ary tree:
+ *
+ *           1
+ *         / |  \
+ *       3   2   4
+ *      / \
+ *     5   6
+ * 
+ * 
+ *
+ * Return its postorder traversal as: [5,6,3,2,4,1].
+ * 
+ *
+ * Note:
+ * Recursive solution is trivial, could you do it iteratively?
+ */
+
+class Solution {
+  public List<Integer> postorder(Node root) {
+    return null;
+  }
+}
