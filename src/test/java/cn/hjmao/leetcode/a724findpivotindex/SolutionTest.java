cb8e9f185b1e806faba882c434293e3301a4fa9e
Huajian Mao <huajianmao@gmail.com>
2019-06-26 14:56:06 +0800

    Add solution for problem 724

---
+++ src/test/java/cn/hjmao/leetcode/a724findpivotindex/SolutionTest.java
@@ -0,0 +1,73 @@
+package cn.hjmao.leetcode.a724findpivotindex;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SolutionTest {
+  private Solution solution = new Solution();
+
+  @Test
+  public void testPivotIndex1() {
+    int[] nums = {1, 7, 3, 6, 5, 6};
+    int expect = 3;
+    int actual = solution.pivotIndex(nums);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testPivotIndex2() {
+    int[] nums = {1, 2, 3};
+    int expect = -1;
+    int actual = solution.pivotIndex(nums);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testPivotIndex3() {
+    int[] nums = {1, 2};
+    int expect = -1;
+    int actual = solution.pivotIndex(nums);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testPivotIndex4() {
+    int[] nums = {-1, -1, -1, -1, -1, 0};
+    int expect = 2;
+    int actual = solution.pivotIndex(nums);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testPivotIndex5() {
+    int[] nums = {-1};
+    int expect = -1;
+    int actual = solution.pivotIndex(nums);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testPivotIndex6() {
+    int[] nums = {2, 2};
+    int expect = -1;
+    int actual = solution.pivotIndex(nums);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testPivotIndex7() {
+    int[] nums = {};
+    int expect = -1;
+    int actual = solution.pivotIndex(nums);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testPivotIndex8() {
+    int[] nums = {-1, -1, -1, 0, 1, 1};
+    int expect = 0;
+    int actual = solution.pivotIndex(nums);
+    assertEquals(actual, expect);
+  }
+}
