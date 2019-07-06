4df657f968f978ed305d35d45f9ad5a6f795f5c4
Huajian Mao <huajianmao@gmail.com>
2019-07-06 21:34:45 +0800

    Add solution for problem 783

---
+++ src/test/java/cn/hjmao/leetcode/a783minimumdistancebetweenbstnodes/SolutionTest.java
@@ -0,0 +1,20 @@
+package cn.hjmao.leetcode.a783minimumdistancebetweenbstnodes;
+
+import cn.hjmao.utils.tree.TreeNode;
+import cn.hjmao.utils.tree.TreeNodeUtils;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SolutionTest {
+  private Solution solution = new Solution();
+
+  @Test
+  public void testMinDiffInBST1() {
+    Integer[] nums = {4, 2, 6, 1, 3};
+    TreeNode root = TreeNodeUtils.fromArray(nums);
+    int expect = 1;
+    int actual = solution.minDiffInBST(root);
+    assertEquals(actual, expect);
+  }
+}
