afa569cc2c0a56134831d3ead5288ee9453977a2
Huajian Mao <huajianmao@gmail.com>
2019-07-16 08:52:58 +0800

    Add solution for problem 832

---
+++ src/test/java/cn/hjmao/leetcode/a832flippinganimage/SolutionTest.java
@@ -0,0 +1,64 @@
+package cn.hjmao.leetcode.a832flippinganimage;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SolutionTest {
+  private Solution solution = new Solution();
+
+  @Test
+  public void testFlipAndInvertImage1() {
+    int[][] a = {
+        {1, 1, 0},
+        {1, 0, 1},
+        {0, 0, 0}
+    };
+    int[][] expect = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {1, 1, 1}
+    };
+    int[][] actual = solution.flipAndInvertImage(a);
+    assertEquals(actual.length, expect.length);
+    for (int i = 0; i < actual.length; i++) {
+      assertEquals(actual[i], expect[i]);
+    }
+  }
+
+  @Test
+  public void testFlipAndInvertImage2() {
+    int[][] a = {
+        {1, 1, 0, 0},
+        {1, 0, 0, 1},
+        {0, 1, 1, 1},
+        {1, 0, 1, 0}
+    };
+    int[][] expect = {
+        {1, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 1},
+        {1, 0, 1, 0}
+    };
+    int[][] actual = solution.flipAndInvertImage(a);
+    assertEquals(actual.length, expect.length);
+    for (int i = 0; i < actual.length; i++) {
+      assertEquals(actual[i], expect[i]);
+    }
+  }
+
+  @Test
+  public void testFlipAndInvertImage3() {
+    int[][] a = {
+        {1}
+    };
+    int[][] expect = {
+        {0}
+    };
+    int[][] actual = solution.flipAndInvertImage(a);
+    assertEquals(actual.length, expect.length);
+    for (int i = 0; i < actual.length; i++) {
+      assertEquals(actual[i], expect[i]);
+    }
+  }
+}
