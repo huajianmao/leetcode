423df108e0af1064554798d73f79c954d087507f
Huajian Mao <huajianmao@gmail.com>
2019-08-15 08:40:14 +0800

    Add solution for problem 917

---
+++ src/main/java/com/leetcode/snippets/a917reverseonlyletters/Solution.java
@@ -0,0 +1,40 @@
+package com.leetcode.snippets.a917reverseonlyletters;
+
+/**
+ * Created by hjmao.
+ *
+ * URL:
+ * =====
+ * https://leetcode.com/problems/reverse-only-letters/
+ *
+ * Desc:
+ * =====
+ * Given a string S, return the "reversed" string
+ * where all characters that are not a letter stay in the same place, and all letters reverse their positions.
+ *
+ *
+ *
+ * Example 1:
+ * Input: "ab-cd"
+ * Output: "dc-ba"
+ *
+ * Example 2:
+ * Input: "a-bC-dEf-ghIj"
+ * Output: "j-Ih-gfE-dCba"
+ *
+ * Example 3:
+ * Input: "Test1ng-Leet=code-Q!"
+ * Output: "Qedo1ct-eeLg=ntse-T!"
+ *
+ *
+ * Note:
+ * S.length <= 100
+ * 33 <= S[i].ASCIIcode <= 122
+ * S doesn't contain \ or ""
+ */
+
+class Solution {
+  public String reverseOnlyLetters(String s) {
+    return null;
+  }
+}
