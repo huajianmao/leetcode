3576661e9b351e0bea449dbeb857e2a27b4cbea5
Huajian Mao <huajianmao@gmail.com>
2019-08-27 08:44:06 +0800

    Add solution for problem 961

---
+++ src/main/java/cn/hjmao/leetcode/a961nrepeatedelementinsize2narray/Solution.java
@@ -0,0 +1,46 @@
+package cn.hjmao.leetcode.a961nrepeatedelementinsize2narray;
+
+/**
+ * Created by hjmao.
+ *
+ * URL:
+ * =====
+ * https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+ *
+ * Desc:
+ * =====
+ * In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+ *
+ * Return the element repeated N times.
+ *
+ *
+ *
+ * Example 1:
+ * Input: [1,2,3,3]
+ * Output: 3
+ *
+ * Example 2:
+ * Input: [2,1,2,5,3,2]
+ * Output: 2
+ *
+ * Example 3:
+ * Input: [5,1,5,2,5,3,5,4]
+ * Output: 5
+ *
+ *
+ * Note:
+ * 4 <= A.length <= 10000
+ * 0 <= A[i] < 10000
+ * A.length is evenn
+ */
+
+class Solution {
+  public int repeatedNTimes(int[] a) {
+    for (int i = 2; i < a.length; i++) {
+      if (a[i] == a[i - 1] || a[i] == a[i - 2]) {
+        return a[i];
+      }
+    }
+    return a[0];
+  }
+}
