10832f46c75fe78e91a23d5534bc7aaeb2ab9285
Huajian Mao <huajianmao@gmail.com>
2019-09-09 14:04:11 +0800

    Add solution for problem 1018

---
+++ src/test/java/cn/hjmao/leetcode/a1018binaryprefixdivisibleby5/SolutionTest.java
@@ -0,0 +1,45 @@
+package cn.hjmao.leetcode.a1018binaryprefixdivisibleby5;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class SolutionTest {
+  private Solution solution = new Solution();
+
+  @Test
+  public void testPrefixesDivBy5T1() {
+    int[] a = {0, 1, 1};
+    List<Boolean> expect = Arrays.asList(true, false, false);
+    List<Boolean> actual = solution.prefixesDivBy5(a);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testPrefixesDivBy5T2() {
+    int[] a = {1, 1, 1};
+    List<Boolean> expect = Arrays.asList(false, false, false);
+    List<Boolean> actual = solution.prefixesDivBy5(a);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testPrefixesDivBy5T3() {
+    int[] a = {0, 1, 1, 1, 1, 1};
+    List<Boolean> expect = Arrays.asList(true, false, false, false, true, false);
+    List<Boolean> actual = solution.prefixesDivBy5(a);
+    assertEquals(actual, expect);
+  }
+
+  @Test
+  public void testPrefixesDivBy5T4() {
+    int[] a = {1, 1, 1, 0, 1};
+    List<Boolean> expect = Arrays.asList(false, false, false, false, false);
+    List<Boolean> actual = solution.prefixesDivBy5(a);
+    assertEquals(actual, expect);
+  }
+}
